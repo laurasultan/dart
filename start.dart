5bffa18ee7a7e9a5e451d9ac20e6e63f8cdfe041
laura <105574779+laurasultan@users.noreply.github.com>
2023-01-31 11:54:11 -0300

    Start

---
+++ start.dart
@@ -0,0 +1,19 @@
+void main(){
+  /*
+  print("Hello world!");
+  print(10);
+  print(10 + 10);
+  
+  int a = 105;
+  double b = 2.8;
+  String c = "Dois";
+  bool d = false;
+  
+  print("$a - $b - $c - $d");
+  */
+  
+  bool testA = (10 > 9);
+  bool testB = (5 == 5.1);
+  
+  print("$testA / $testB");
+}
