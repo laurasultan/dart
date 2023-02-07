cbb8645c69220ccac6d8ac58877d747d65f72396
laura <105574779+laurasultan@users.noreply.github.com>
2023-02-07 10:03:07 -0300

    Create aula3.dart

---
+++ aula3.dart
@@ -0,0 +1,14 @@
+void main() {
+  repeteFrase(0, 10, "Será que funciona?");
+}
+
+void repeteFrase(int i, int y, String text) {   //parâmetros
+  for(i; i < y; i++){   //+=2
+  //variável (atribuição), expressão booleana, incremento
+    print(text);         
+  }
+}
+
+----------------------------------------------------------------------------------------------------------
+  
+  
