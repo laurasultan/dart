f12597119397cb4865b0d0a81a5ed90620717f63
laura <105574779+laurasultan@users.noreply.github.com>
2023-02-14 11:52:02 -0300

    Counter
    Widget Tree, Material App, Scaffold (appBar, body, bottomNavigationBar).
    Center, container, padding (child).
    Row, column, stack (children).

--- aula5.dart
+++ aula5.dart
@@ -47,3 +47,106 @@ class Animal {
   String toString() =>
     "Espécie: $_especie\nIdade: $_idade anos\nPeso: $_peso kg";
 }
+
+-------------------------------------------- exemplo Counter ---------------------------------------------------
+  
+// Copyright (c) 2019, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
+      ),
+      home: const MyHomePage(title: 'Contador'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  //Atributo
+  final String title;
+
+  //Construtor
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+  
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+  
+  void _clearCounter() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Nome que eu quiser"),
+        centerTitle: true,
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'CONTADOR',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Mais',
+              child: const Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: _clearCounter,
+              tooltip: 'Zerar',
+              child: const Icon(Icons.clear),
+            ),
+            FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'Menos',
+              child: const Icon(Icons.remove),
+            ),
+          ]
+        ),
+
+    ); //Scaffold
+  }
+}
